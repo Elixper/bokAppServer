0cce9b1c26f75335da1fbcd183274c46784ab19d
Aurelie75FR <aurelielo.vi@gmail.com>
2021-06-07 11:29:27 +0200

    modif sur list route en then catch promise

--- routes/list.routes.js
+++ routes/list.routes.js
@@ -16,27 +16,30 @@ router.get("/:id", (req, res, next) => {
 });
 
 router.post("/add-list", (req, res, next) => {
-    const list = new List();
-    list.title = req.body.title;
-    list.authors = req.body.authors;
-    list.genre = req.body.genre;
-    list.publishedDate = req.body.publishedDate;
-    list.description = req.body.description;
-    list.thumbnail = req.body.thumbnail;
-    list.price = req.body.price;
-    list.purchase = req.body.purchase;
-    list.user_id = req.session.currentUser
-  
-    list.save(function (err) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.json({
-          message: "added to your list",
-          list: list,
-        });
-      }
-    });
-  });
+  const list = new List();
+  list.title = req.body.title;
+  list.authors = req.body.authors;
+  list.genre = req.body.genre;
+  list.publishedDate = req.body.publishedDate;
+  list.description = req.body.description;
+  list.thumbnail = req.body.thumbnail;
+  list.price = req.body.price;
+  list.purchase = req.body.purchase;
+  list.user_id = req.session.currentUser;
+
+  // list.save(function (err) {
+  //   if (err) {
+  //     res.send(err);
+  //   } else {
+  //     res.json({
+  //       message: "added to your list",
+  //       list: list,
+  //     });
+  //   }
+  // });
+  list.save()
+    .then((res) => res.json({ message: "added to your list", list: list }))
+    .catch(next);
+});
 
 module.exports = router;
