99791109649f27392cabc0b97fb8893137634b0e
Aurelie75FR <aurelielo.vi@gmail.com>
2021-06-07 14:51:18 +0200

    changement dans model et route list

--- models/List.js
+++ models/List.js
@@ -2,15 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listSchema = new Schema({
-  title: String,
+  title: [{type:String}],
   authors: String,
-  genre: String,
+  categories: [{type:String}],
   publishedDate: String,
   description: String,
   thumbnail: {type:String,
   default: "https://www.sortiraparis.com/images/1002/1665/547990-coronavirus-comment-se-procurer-des-livres-pendant-le-confinement.jpg"},
+  //amount dans listPrice
   price: Number,
+//purchase = buyLink dans saleInfo
   purchase: String,
+  //pageCount
 });
 
 const List = mongoose.model("List", listSchema);
