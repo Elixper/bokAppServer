3dec2fbf343c0a92988a1c4ec8d983a820feb632
Aurelie75FR <aurelielo.vi@gmail.com>
2021-06-08 13:30:30 +0200

    des modif a venir

--- middlewares/isAuthorRoute.js
+++
@@ -1,6 +0,0 @@
-module.exports = function isAuthorRoute(req, res, next) {
-    if (req.session.currentUser && req.session.currentUser.role === true)
-      next();
-    else res.redirect("/my-account");
-  };
-  
--- routes/book.routes.js
+++ routes/book.routes.js
@@ -4,105 +4,99 @@ const BokBook = require("../models/BoKBook");
 const uploader = require("../config/cloudinary");
 //const requireAuth = require("../middlewares/requireAuth")
 
-
 //router.get all
 
 router.get("/", (req, res, next) => {
-    BokBook.find({}) 
-      .populate("author") 
-      .then((itemBook) => {
-        res.status(200).json(itemBook);
-      })
-      .catch(next); 
-  });
+  BokBook.find({})
+    .populate("author")
+    .then((itemBook) => {
+      res.status(200).json(itemBook);
+    })
+    .catch(next);
+});
 
 //router.get one
 
-router.get("/:id", (req, res, next) => {
-    BokBook.findById(req.params.id) 
-      .populate("author") 
-      .then((itemDocuments) => {
-        res.status(200).json(itemDocuments);
-      })
-      .catch(next); 
-  });
+// router.get("/:id", (req, res, next) => {
+//   BokBook.findById(req.params.id)
+//     .populate("author")
+//     .then((itemDocuments) => {
+//       res.status(200).json(itemDocuments);
+//     })
+//     .catch(next);
+// });
+//router.post create
+
+router.post("/your-masterpiece", uploader.single("image"), (req, res, next) => {
+  const updateValues = { ...req.body };
+
+  if (req.file) {
+    updateValues.image = req.file.path;
+  }
+
+  updateValues.author = req.session.currentUser;
+
+  BokBook.create(updateValues)
+    .then((itemDocument) => {
+      itemDocument
+        .populate("author")
+        .execPopulate()
+        .then((item) => {
+          console.log("here");
+          res.status(201).json(item);
+        })
+        .catch(next);
+    })
+    .catch(next);
+});
 
 //router.patch
 
-router.patch(
-    "/change/:id",
-    (req, res, next) => {
-      const item = { ...req.body };
-  
-      BokBook.findById(req.params.id)
-        .then((itemBook) => {
-          if (!itemBook)
-            return res.status(404).json({ message: "Item not found" });
-          if (itemDocument.author.toString() !== req.session.currentUser) {
-            return res
-              .status(403)
-              .json({ message: "You are not allowed to update this document" });
-          }
-  
-          if (req.file) {
-            item.image = req.file.secure_url;
-          }
-  
-          BokBook.findByIdAndUpdate(req.params.id, item, { new: true })
-            .populate("author")
-            .then((updatedDocument) => {
-              return res.status(200).json(updatedDocument);
-            })
-            .catch(next);
+router.patch("/:id", uploader.single("image"), (req, res, next) => {
+  const item = { ...req.body };
+
+  BokBook.findById(req.params.id)
+    .then((itemBook) => {
+      if (!itemBook) return res.status(404).json({ message: "Bok not found" });
+      if (itemDocument.author.toString() !== req.session.currentUser) {
+        return res
+          .status(403)
+          .json({ message: "You are not allowed to update this document" });
+      }
+
+      if (req.file) {
+        item.image = req.file.secure_url;
+      }
+
+      BokBook.findByIdAndUpdate(req.params.id, item, { new: true })
+        .populate("author")
+        .then((updatedDocument) => {
+          return res.status(200).json(updatedDocument);
         })
         .catch(next);
-    }
-  );
+    })
+    .catch(next);
+});
 
 //router.delete
 
-router.delete("/delete/:id", (req, res, next) => {
-    BokBook.findById(req.params.id)
-      .then((itemDocument) => {
-        if (!itemDocument) {
-          return res.status(404).json({ message: "Item not found" });
-        }
-        if (itemDocument.creator.toString() !== req.session.currentUser) {
-          return res.status(403).json({ message: "You can't delete this item" });
-        }
-  
-        BokBook.findByIdAndDelete(req.params.id)
-          .then(() => {
-            return res.sendStatus(204);
-          })
-          .catch(next);
-      })
-      .catch(next);
-  });
+router.delete("/:id", (req, res, next) => {
+  BokBook.findById(req.params.id)
+    .then((itemDocument) => {
+      if (!itemDocument) {
+        return res.status(404).json({ message: "bok not found" });
+      }
+      if (itemDocument.author.toString() !== req.session.currentUser) {
+        return res.status(403).json({ message: "You can't delete this item" });
+      }
 
-  
-
-//router.post create
-
-router.post("/your-masterpiece", (req, res, next) => {
-    const updateValues = { ...req.body };
-  
-    
-    updateValues.author = req.session.currentUser; 
-  
-    BokBook.create(updateValues)
-      .then((itemDocument) => {
-        itemDocument
-          .populate("author")
-          .execPopulate() 
-          .then((item) => {
-            console.log("here");
-            res.status(201).json(item); 
-          })
-          .catch(next);
-      })
-      .catch(next);
-  });
-
+      BokBook.findByIdAndDelete(req.params.id)
+        .then(() => {
+          return res.sendStatus(204);
+        })
+        .catch(next);
+    })
+    .catch(next);
+});
 
 module.exports = router;
